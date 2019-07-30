6b006a2bfe35013ee2e90ca510f77ab8411dc89f
siexp <elioni17@gmail.com>
2019-07-30 15:17:07 +0300

    review

--- contracts/contracts/rem.system/src/voting.cpp
+++ contracts/contracts/rem.system/src/voting.cpp
@@ -86,11 +86,9 @@ namespace eosiosystem {
    void system_contract::unregprod( const name& producer ) {
       require_auth( producer );
 
-      const auto voter = _voters.find( producer.value );
-      
-      check( voter != _voters.end(), "producer is not found in voter table" );
-      check( voter->vote_mature_time <= current_time_point(), "producers are not allowed to unreg while stake lock period" );
-      
+      const auto& voter = _voters.get( producer.value, "producer is not found in voter table" );
+      check( voter.vote_mature_time <= current_time_point(), "producers are not allowed to unregister during stake lock period" );
+
       claimrewards(producer);
 
       const auto& prod = _producers.get( producer.value, "producer not found" );
