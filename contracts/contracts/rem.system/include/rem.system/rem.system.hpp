54a33fa620a8e5efa0548db180c3f18d7410c557
Vadim <vadym.pysarenko@remme.io>
2019-10-11 17:21:15 +0300

    remove unused constants

--- contracts/contracts/rem.system/include/rem.system/rem.system.hpp
+++ contracts/contracts/rem.system/include/rem.system/rem.system.hpp
@@ -605,9 +605,7 @@ namespace eosiosystem {
          static constexpr eosio::name saving_account{"rem.saving"_n};
          static constexpr eosio::name rex_account{"rem.rex"_n};
          static constexpr eosio::name null_account{"rem.null"_n};
-         static constexpr symbol ramcore_symbol = symbol(symbol_code("RAMCORE"), 4);
-         static constexpr symbol ram_symbol     = symbol(symbol_code("RAM"), 0);
-         static constexpr symbol rex_symbol     = symbol(symbol_code("REX"), 4);
+         static constexpr symbol rex_symbol = symbol(symbol_code("REX"), 4);
 
          static constexpr uint8_t max_block_producers      = 21;
          
