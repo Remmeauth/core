7cd79a8d7a323718a1b2c4defd49dea2a3177c91
siexp <elioni17@gmail.com>
2019-10-11 15:01:58 +0300

    locking rules

--- contracts/contracts/rem.system/include/rem.system/rem.system.hpp
+++ contracts/contracts/rem.system/include/rem.system/rem.system.hpp
@@ -1072,6 +1072,17 @@ namespace eosiosystem {
          [[eosio::action]]
          void refund( const name& owner );
 
+         /**
+          * Refund to stake action.
+          *
+          * @details This action is called after the delegation-period to restake without lock all pending
+          * unstaked tokens belonging to owner.
+          *
+          * @param owner - the owner of the tokens claimed.
+          */
+         [[eosio::action]]
+         void refund1( const name& owner );
+
          // functions defined in voting.cpp
 
          /**
@@ -1346,6 +1357,7 @@ namespace eosiosystem {
          using closerex_action = eosio::action_wrapper<"closerex"_n, &system_contract::closerex>;
          using undelegatebw_action = eosio::action_wrapper<"undelegatebw"_n, &system_contract::undelegatebw>;
          using refund_action = eosio::action_wrapper<"refund"_n, &system_contract::refund>;
+         using refund1_action = eosio::action_wrapper<"refund1"_n, &system_contract::refund1>;
          using regproducer_action = eosio::action_wrapper<"regproducer"_n, &system_contract::regproducer>;
          using unregprod_action = eosio::action_wrapper<"unregprod"_n, &system_contract::unregprod>;
          using setram_action = eosio::action_wrapper<"setram"_n, &system_contract::setram>;
