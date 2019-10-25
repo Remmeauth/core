842d386abf57b5c6fabed01a2bf5cdd24586a736
Vadim <vadym.pysarenko@remme.io>
2019-10-25 12:10:58 +0300

    change eosio->rem

--- tests/nodeos_chainbase_allocation_test.py
+++ tests/nodeos_chainbase_allocation_test.py
@@ -49,7 +49,7 @@ try:
     # - permission_object (bootstrap)
     # The bootstrap process has created account_object and code_object (by uploading the bios contract),
     # key_value_object (token creation), protocol_state_object (preactivation feature), and permission_object
-    # (automatically taken care by the automatically generated eosio account)
+    # (automatically taken care by the automatically generated rem account)
     assert cluster.launch(
         pnodes=1,
         prodCount=1,
@@ -66,8 +66,8 @@ try:
     irrNode = cluster.getNode(irrNodeId)
 
     # Create delayed transaction to create "generated_transaction_object"
-    cmd = "create account -j eosio sample EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV\
-         EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV --delay-sec 600 -p eosio"
+    cmd = "create account -j rem sample EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV\
+         EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV --delay-sec 600 -p rem"
     trans = producerNode.processCleosCmd(cmd, cmd, silentErrors=False)
     assert trans
 
@@ -80,7 +80,7 @@ try:
     setProdsStr = '{"schedule": ['
     setProdsStr += '{"producer_name":' + newProducerAcc.name + ',"block_signing_key":' + newProducerAcc.activePublicKey + '}'
     setProdsStr += ']}'
-    cmd="push action -j eosio setprods '{}' -p eosio".format(setProdsStr)
+    cmd="push action -j rem setprods '{}' -p rem".format(setProdsStr)
     trans = producerNode.processCleosCmd(cmd, cmd, silentErrors=False)
     assert trans
     setProdsBlockNum = int(trans["processed"]["block_num"])
