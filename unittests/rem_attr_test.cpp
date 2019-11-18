3cccda989d816b24b6a47f51051191be167e68f4
Vadim <vadym.pysarenko@remme.io>
2019-11-18 11:11:16 +0200

    fix attribute test

--- unittests/rem_attr_test.cpp
+++ unittests/rem_attr_test.cpp
@@ -65,6 +65,11 @@ struct set_attr_expected_t : public set_attr_t {
       bytes b(actualValue.size() / 2);
       const auto size = fc::from_hex(actualValue, b.data(), b.size());
 
+      if (std::is_integral<T>::value) {
+         for (size_t i = 0; i < b.size() / 2; i++) {
+            std::swap(b[i], b[b.size() - (1 + i)]);
+         }
+      }
       const auto v = fc::raw::unpack<T>(b);
       return v == expectedValue;
    }
@@ -353,7 +358,7 @@ BOOST_FIXTURE_TEST_CASE( attribute_test, attribute_tester ) {
             "06426c61697a65", // encoded string "Blaize"
             "Blaize");
         set_attr_expected_t<int64_t> attr8(N(proda), N(prodb), N(largeint),
-            "0000000000000080", // encoded number -9223372036854775808
+            "8000000000000000", // encoded number -9223372036854775808
             -9223372036854775808);
         set_attr_expected_t<int64_t> attr9(N(prodb), N(prodb), N(largeint),
            "ffffffffffffffff", // encoded number -1
