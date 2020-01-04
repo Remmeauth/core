8ad1538b9878c232ba9a9110281a2ba6c78fb1e4
Vlad Pavlichek <ember.toon@protonmail.com>
2020-01-04 09:28:00 +0200

    add tests

--- unittests/rem_account_price_test.cpp
+++ unittests/rem_account_price_test.cpp
@@ -55,6 +55,98 @@ BOOST_FIXTURE_TEST_CASE(rem_account_price_test, rem_system::eosio_system_tester)
 
             produce_blocks(1);
         }
+
+       // everyone should be able to create account with given price
+       {
+          const int64_t account_price = 1'0000;
+          push_action( config::system_account_name, N(setminstake), mvo()("min_account_stake", account_price) );
+
+          const auto min_account_stake = get_global_state()["min_account_stake"].as<int64_t>();
+          BOOST_REQUIRE_EQUAL(min_account_stake, account_price);
+
+          create_account_with_resources(
+             N(testuser333),
+             config::system_account_name,
+             false,
+             asset{ account_price }
+          );
+
+          produce_blocks(1);
+       }
+
+       // everyone should be able to create account with given price
+       {
+          const int64_t account_price = 1;
+          push_action( config::system_account_name, N(setminstake), mvo()("min_account_stake", account_price) );
+
+          const auto min_account_stake = get_global_state()["min_account_stake"].as<int64_t>();
+          BOOST_REQUIRE_EQUAL(min_account_stake, account_price);
+
+          create_account_with_resources(
+             N(testuser335),
+             config::system_account_name,
+             false,
+             asset{ account_price }
+          );
+
+          produce_blocks(1);
+       }
+
+       // ram_gift_bytes should be equal 0 in this tests
+       // everyone should be able to create account with given price
+       {
+          const int64_t account_price = 100'0000;
+          push_action( config::system_account_name, N(setminstake), mvo()("min_account_stake", account_price) );
+
+          const auto min_account_stake = get_global_state()["min_account_stake"].as<int64_t>();
+          BOOST_REQUIRE_EQUAL(min_account_stake, account_price);
+
+          create_account_with_resources(
+             N(testuser444),
+             config::system_account_name,
+             false,
+             asset{ account_price }
+          );
+
+          produce_blocks(1);
+       }
+
+       // everyone should be able to create account with given price
+       {
+          const int64_t account_price = 1000'0000;
+          push_action( config::system_account_name, N(setminstake), mvo()("min_account_stake", account_price) );
+
+          const auto min_account_stake = get_global_state()["min_account_stake"].as<int64_t>();
+          BOOST_REQUIRE_EQUAL(min_account_stake, account_price);
+
+          create_account_with_resources(
+             N(testuser555),
+             config::system_account_name,
+             false,
+             asset{ account_price }
+          );
+
+          produce_blocks(1);
+       }
+
+       // everyone should be able to create account with given price
+       {
+          const int64_t account_price = 10000'0000;
+          push_action( config::system_account_name, N(setminstake), mvo()("min_account_stake", account_price) );
+
+          const auto min_account_stake = get_global_state()["min_account_stake"].as<int64_t>();
+          BOOST_REQUIRE_EQUAL(min_account_stake, account_price);
+
+          create_account_with_resources(
+             N(testuser511),
+             config::system_account_name,
+             false,
+             asset{ account_price }
+          );
+
+          produce_blocks(1);
+       }
     } FC_LOG_AND_RETHROW()
 }
+
 BOOST_AUTO_TEST_SUITE_END()
